12188dae38349c7929384b5b22dd273ef0027e63
IsraeelP7 <israelprieto18@aragon.unam.mx>
2023-11-30 01:01:07 -0600

    pagina que muestra la lista de usuarios con mejor vista

--- dashboard.php
+++ dashboard.php
@@ -1,51 +1,66 @@
 <?php
-// se usa el requiere para si psi se necesita el archivo conexion
+// Se usa "require" para incluir el archivo de conexión
 require "conexion.php";
-mysqli_set_charset($conexion,'utf8');
+mysqli_set_charset($conexion, 'utf8');
 
+// Generar la consulta SQL
+$consulta_sql = "SELECT * FROM usuarios";
 
-//genear el query
-$consulta_sql="SELECT * FROM usuarios";
-
-//mandar el query por medio de la conexion y almacenaremos el resultado en una variable
+// Mandar el query por medio de la conexión y almacenaremos el resultado en una variable
 $resultado = $conexion->query($consulta_sql);
 
-// Retorna el numero de filas del resultado. Si encuentra mas de uno lo usamos para imprimir el resultado en nuestra tabla
-$count = mysqli_num_rows($resultado); 
- 
-echo "<table border='2' >
-    <tr>
-        <th>ID</th>
-        <th>Nombre</th>
-        <th>Apelldo</th>
-        <th>Telefono</th>
-        <th>Usuario</th>
-    
-    </tr>";
-
-if ( $count>0 ){
-    //aqui se pintarian los registro de la DB
-    while( $row = mysqli_fetch_assoc($resultado)  ){
-     echo "<tr>";
-     echo"<td>". $row['id'] ."</td>";
-     echo"<td>". $row['nombre'] ."</td>";
-     echo"<td>". $row['apellido'] ."</td>";
-     echo"<td>". $row['telefono'] ."</td>";
-     echo"<td>". $row['usuario'] ."</td>";
-     echo "</tr>";
-     
-    }
-    echo "</table>";
-
-}else{
-    
-    
-    echo"<h1 style='color:red' >Sin Ningun registro</h1>";
-    }
-
-    echo "<h1><a href='EliminarUsuario.php'>¿Deseas eliminar tu usuario?</a></h1>";
-    echo "<h3><a href='index.html'>Pagina principal</a></h3>";
+// Retorna el numero de filas del resultado. Si encuentra más de uno, lo usamos para imprimir el resultado en nuestra tabla
+$count = mysqli_num_rows($resultado);
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Usuarios</title>
+    <link rel="stylesheet" href="style5.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Lista de Usuarios</h1>
+
+        <?php if ($count > 0) : ?>
+            <!-- Si hay registros, mostramos la tabla -->
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Teléfono</th>
+                        <th>Usuario</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = mysqli_fetch_assoc($resultado)) : ?>
+                        <tr>
+                            <td><?php echo $row['id']; ?></td>
+                            <td><?php echo $row['nombre']; ?></td>
+                            <td><?php echo $row['apellido']; ?></td>
+                            <td><?php echo $row['telefono']; ?></td>
+                            <td><?php echo $row['usuario']; ?></td>
+                        </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+        <?php else : ?>
+            <!-- Si no hay registros, mostramos un mensaje -->
+            <h1 class="no-records">Sin ningún registro</h1>
+        <?php endif; ?>
+
+        <!-- Enlaces de navegación -->
+        <h1><a href='EliminarUsuario.php'>¿Deseas eliminar tu usuario?</a></h1>
+        <h3><a href='index.html'>Página principal</a></h3>
+    </div>
+</body>
+
+</html>
